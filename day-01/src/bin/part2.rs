55264a9f7c7ed0bcc5d8f98b9b29ef4d7fa965ec
Paden Rose <paden.rose@q2.com>
2023-12-01 17:19:44 -0600

    WIP: changing to lazy_static hashmap

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -1,4 +1,24 @@
+#[macro_use]
+extern crate lazy_static;
+
 use std::collections::HashMap;
+
+lazy_static! {
+static ref WORD_TO_NUMBER: HashMap<&'static str, u32> = {
+        let mut m = HashMap::new();
+        m.insert("one", 1);
+        m.insert("two", 2);
+        m.insert("three", 3);
+        m.insert("four", 4);
+        m.insert("five", 5);
+        m.insert("six", 6);
+        m.insert("seven", 7);
+        m.insert("eight", 8);
+        m.insert("nine", 9);
+        m.insert("zero", 0);
+        m
+    };
+}
 
 fn main() {
     let input = include_str!("./day-01-input.txt");
@@ -6,12 +26,13 @@ fn main() {
     println!("{}", total);
 }
 
-fn word_to_number_conversion(word: &str) -> &str{
-    let word_to_number = HashMap::new();
+
+fn word_to_number_conversion(word: &str) -> &str {
     let mut new_word = "";
     for char in word.chars() {
-        new_word = format!("{}{}", new_word, char).as_str();
-        match new_word.contains()
+        println!("{}", char);
+        // new_word = format!("{}{}", new_word, char).as_str();
+        // match new_word.contains(WORD_TO_NUMBER.iter())
     }
     new_word
 }
